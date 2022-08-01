900e12a194f0de5f3a8b7a18378450f4a358cc83
Wangouf <wanghso@foxmail.com>
2022-08-01 14:08:24 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-git clone https://github.com/Wangouf/android_kernel_xiaomi_mt6765.git
 git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b ndk-release-r21
-cd ./android_kernel_xiaomi_mt6765
+#git clone https://github.com/Wangouf/android_kernel_xiaomi_mt6765.git
+#cd ./android_kernel_xiaomi_mt6765
+ls -a
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=../arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-make O=out certus_defconfig
+export CROSS_COMPILE=./arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+#make O=out certus_defconfig
 #make mrproper
-make -j$(nproc) O=out 2>&1 | tee kernel.log
+#make -j$(nproc) O=out 2>&1 | tee kernel.log
